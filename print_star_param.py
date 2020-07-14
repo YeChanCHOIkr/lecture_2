dfb90ad087181c8c99546956265f3ab046e82981
Your Name <you@example.com>
2020-07-14 17:41:21 +0900

    First Commit

---
+++ print_star_param.py
@@ -0,0 +1,5 @@
+def print_star_param(n):
+    for _ in range(n):
+        print('********************************')
+
+print_star_param(4)
---
+++ sum_func.py
@@ -0,0 +1,7 @@
+### 두 개의 매개변수를 가진 함수 ###
+def print_sum(a,b):
+    result = a + b
+    print(a, '과', b, '의 합은', result, '입니다.')
+
+print_sum(10,20)
+print_sum(100,200)
