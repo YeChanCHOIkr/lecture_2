062269f7584400adbbd5875c832557fe57e0dd3b
Your Name <you@example.com>
2020-07-16 10:59:47 +0900

    Second Commit

---
+++ for_data.py
@@ -0,0 +1,10 @@
+import data_dict
+print(data_dict.elementary_school["제공기관명"])
+
+print(data_dict.print_ele())
+
+# import arg_greet
+# import factorial_recursion as fr
+
+# arg_greet.greet()
+# # fr.factorial(10)
